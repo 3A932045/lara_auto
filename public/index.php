dc4d2ce8a661be92879d2fab09c57f67c759479f
DESKTOP-RV1HMT4\Luly_chen <shadowy999@gmail.com>
2022-10-05 00:06:40 +0800

    主網頁設計:命名空間Demo下的類別自動載入及使用

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+	require '../vendor/autoload.php';
+	//類別先 import 進來
+	
+	use Demo\Hello\Lara;
+	use Demo\Hello\Someone;
+	
+	$lara = new Lara();
+	$vincent = new Someone('Vincent');
+	
+	//類別未先 import 進來
+	$mary = new Demo\Hello\Someone('Mary');
+	$john = new Demo\Hello\Someone('John');
+	$hello = new Demo HelloWorld();
+	use Demo\HelloWorld as World;
+	
+	// 類別另取別名
+	$world = new World();
+?>
